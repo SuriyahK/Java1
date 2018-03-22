615c4f3ff3c120a15450325dfcba634eb761fd10
suriyahk <anikamoni@yahoo.com>
2018-03-22 15:46:52 -0400

    added class

---
+++ GitExample/src/Gitclass.java
@@ -0,0 +1,22 @@
+
+public class Gitclass {
+	
+	
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			for (int i = 10; i >8; i--) {  
+				System.out.println("show me result "+i);
+				
+			}
+
+		}
+
+	
+
+	
+	}
+
+
